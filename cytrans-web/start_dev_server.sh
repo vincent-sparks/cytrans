be9022090b25b226e2319e37150124ee731b9b9d
Vincent Sparks <vincent@spacestation.blue>
2025-06-17 23:03:27 -0700

    improvements to the cytrans-web start_dev_server script

--- cytrans-web/start_dev_server.sh
+++ cytrans-web/start_dev_server.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-SERVER_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths .#cytrans-web-server -j$(nproc))
-#STATIC_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths .\#cytrans-web-www -j$(nproc))
-STATIC_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths .\#cytrans-web-www-compressed -j$(nproc))
+CYTRANS_WEB_ROOT=$(dirname $0)
+
+SERVER_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-server -j$(nproc))
+if [[ "$1" == "--compressed" ]]; then 
+    STATIC_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-www-compressed -j$(nproc))
+else
+    CLIENT_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-client -j$(nproc))
+    rm $CYTRANS_WEB_ROOT/www/client
+    ln -s $CLIENT_PATH/client $CYTRANS_WEB_ROOT/www/client
+    STATIC_PATH=$CYTRANS_WEB_ROOT/www
+fi
 
 $SERVER_PATH/bin/server-ng --output-dir . --url-prefix localhost:8080 --static-dir $STATIC_PATH
