491e32ff74d7b47e525820bcb6de4fda064bed7c
Vincent Sparks <vincent@spacestation.blue>
2025-07-08 09:58:36 +0000

    start_dev_server now exits early if a build fails

--- cytrans-web/start_dev_server.sh
+++ cytrans-web/start_dev_server.sh
@@ -2,11 +2,11 @@
 
 CYTRANS_WEB_ROOT=$(dirname $0)
 
-SERVER_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-server -j$(nproc))
+SERVER_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-server -j$(nproc)) || exit
 if [[ "$1" == "--compressed" ]]; then 
-    STATIC_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-www-compressed -j$(nproc))
+    STATIC_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-www-compressed -j$(nproc)) || exit
 else
-    CLIENT_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-client -j$(nproc))
+    CLIENT_PATH=$(nix --extra-experimental-features "nix-command flakes" build --no-link --print-out-paths $CYTRANS_WEB_ROOT/..\#cytrans-web-client -j$(nproc)) || exit
     rm $CYTRANS_WEB_ROOT/www/client
     ln -s $CLIENT_PATH/client $CYTRANS_WEB_ROOT/www/client
     STATIC_PATH=$CYTRANS_WEB_ROOT/www
